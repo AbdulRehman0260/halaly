95687c072b877703922d1b48eecd125518a47ae2
Abdul Rehman Shoukat <abdulzain19@gmail.com>
2025-10-31 22:55:53 +0000

    added ability to create a user in the database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,7 @@ import {drizzle} from 'drizzle-orm/postgres-js';
 import { migrate } from 'drizzle-orm/postgres-js/migrator';
 import express from 'express';
 import { handlerReadiness } from './api/handlers/handlerReadiness.js';
+import { handlerCreateUser } from './api/handlers/handlerCreateUser.js';
 import { middlewareLogging } from './api/middleware/logging.js';
 import { config } from './config.js';
 // Load .env before reading process.env
@@ -15,6 +16,10 @@ const migrationClient = postgres(config.db.url,{max:1});
 await migrate(drizzle(migrationClient), config.db.migrationConfig);
 const app = express();
 app.use(express.json());
+
+
+
+app.post('/api/users',handlerCreateUser);
 app.get('/healthz',middlewareLogging, handlerReadiness);
 app.use("/app",express.static('./src/app'));
 
